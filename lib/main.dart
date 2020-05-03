13d156fadc031cac77e62bbec996a6c3ea866b7c
al64238 <al64238@utad.eu>
2020-05-03 22:28:18 +0100

    done

--- lib/main.dart
+++ lib/main.dart
@@ -11,29 +11,11 @@ void main() => runApp(MyApp());
 /// This Widget is the main application widget.
 class MyApp extends StatelessWidget {
   static const String _title = 'Aplicação Nosso Shopping';
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-<<<<<<< HEAD
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: MyHomePage(title: 'Nosso Shopping'),
-=======
       title: _title,
       home: MyStatelessWidget(),
->>>>>>> be15d3689efccf29708d42cb76faa5fdc6efe868
     );
   }
 }
@@ -46,19 +28,57 @@ void openPage(BuildContext context) {
     builder: (BuildContext context) {
       return Scaffold(
         appBar: AppBar(
+          backgroundColor: Colors.black,
           title: const Text('Registar'),
         ),
-        body: TextField(
-        obscureText: true,
-        decoration: InputDecoration(
-        border: OutlineInputBorder(),
-        labelText: '',
+        body: Center(child: 
+        Column (children: [
+          SizedBox(height: 15),
+          TextFormField(
+         decoration: const InputDecoration(         
+         icon: Icon(Icons.person),
+          hintText: 'Name?',
+          labelText: 'Name *',
+  ),
         ),
-      ),
+        TextFormField(
+          decoration: const InputDecoration(         
+         icon: Icon(Icons.person),
+          hintText: 'Email?',
+          labelText: 'Email*',
+  ),
+        
+
+        ),
+        TextFormField(
+          decoration: const InputDecoration(         
+         icon: Icon(Icons.person),
+          hintText: 'Password?',
+          labelText: 'Password*',
+  ),),
+  TextFormField(
+          decoration: const InputDecoration(         
+         icon: Icon(Icons.person),
+          hintText: 'Confirmar Password?',
+          labelText: 'Confirmar Password*',
+  ),),
+    
+    
+        ]
+
+
+
+        ),
+     
+    
+        ),
+        
       );
     },
   ));
 }
+
+
 
 /// This is the stateless widget that the main application instantiates.
 class MyStatelessWidget extends StatelessWidget {
